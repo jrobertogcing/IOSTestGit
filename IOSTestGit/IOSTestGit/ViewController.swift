d127827ff40d972d3ac9bcef5ed141f48d95108b
J. Roberto <jose.gonzalez@gonet.us>
2019-01-23 09:53:30 -0600

    Fist Commit. Welcome Label

---
+++ IOSTestGit/IOSTestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSTestGit
+//
+//  Created by Jose González on 1/23/19.
+//  Copyright © 2019 Jose Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
